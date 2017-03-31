605e57ad056d450249a81edf8f14bdd71c694d67
blackbbc <505968815@qq.com>
2017-03-31 23:50:42 +0800

    fallback to frontend

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import os
 
 from werkzeug.contrib.fixers import ProxyFix
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from api import bp
 from util import json
 
@@ -13,8 +13,17 @@ app.secret_key = os.urandom(24)
 app.json_encoder = json.MongoJSONEncoder
 app.register_blueprint(bp, url_prefix='/api')
 
-# app.wsgi_app = ProxyFix(app.wsgi_app)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+    # return render_template('index.html')
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+    # return redirect(url_for("index"))
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
