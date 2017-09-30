13d2cf8bb7afc108e45a3fe220bc52a6c4ff4c87
Breezewish <me@breeswish.org>
2017-09-30 18:30:46 +0800

    update FE

--- copy-fe.sh
+++ copy-fe.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cd ../TJLMS-FE
-git pull
-cd ../TJLMS
-cp ../TJLMS-FE/dist/main.js src/static/
-cp ../TJLMS-FE/dist/style.css src/static/
-cp ../TJLMS-FE/dist/vendor.css src/static/
-cp ../TJLMS-FE/dist/vendor.js src/static/
-cp ../TJLMS-FE/dist/index.html src/templates/
+rm -rf src/static/*
+rm -f src/templates/*
+cp -R ../TJLMS-FE2/dist/static/* src/static/
+cp ../TJLMS-FE2/dist/index.html src/templates/
